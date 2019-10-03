8e9d12c99faca81950c26d8bae70cd474e76dcd4
Jessica Sánchez <michelle.sr16@gmail.com>
2019-10-02 23:17:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [apiResponse, setResponse] = useState()
+
+  useEffect(() => {
+    callAPI();
+  }, [])
+
+  function callAPI() {
+    fetch("http://localhost:3007/testAPI")
+        .then(res => res.text())
+        .then(res => setResponse(res));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {apiResponse}
         </p>
         <a
           className="App-link"
@@ -16,7 +28,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
       </header>
     </div>
