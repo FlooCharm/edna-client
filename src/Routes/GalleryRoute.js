53ba6ccb862e196362fa46b2a72bccb3dce35899
Jessica Sánchez <michelle.sr16@gmail.com>
2019-11-22 21:25:51 -0600

    added gallery grid

--- src/Routes/GalleryRoute.js
+++ src/Routes/GalleryRoute.js
@@ -2,22 +2,36 @@ import React, {
 	// useState, 
 	useEffect, 
 } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchSuits } from '../actions/SuitsActions';
 
 export default function GalleryRoute() { 
 	const dispatch = useDispatch();
+	const suits = useSelector(state => state.Suits.allIds.map(id => state.Suits.byId[id]));
 
 	useEffect(() => {
 		dispatch(fetchSuits())
 	}, [])
 
 	return (
-		<div className="container flex column align-items-center">
-			<div className='flex column full-width align-items-center'>
-				<p className='huge-text'>GALERÍA</p>	
+		suits ? (
+			<div className="container flex column align-items-center">
+				<div className='flex column full-width align-items-center'>
+					<p className='huge-text'>GALERÍA</p>
+					<div className='gallery-container'>
+						{
+							suits.map(suit => (
+								<div key={suit._id} className='gallery-card grey-bg'>
+									<img src={suit.thumbnail} width='100%'/>
+								</div>
+							))
+						}
+					</div>
+				</div>
 			</div>
-		</div>
+		) : (
+			<div>Loading...</div>
+		)
 	)	
 }
