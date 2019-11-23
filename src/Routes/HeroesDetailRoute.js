ecc67abc439aaf7fcbd0b3dcc3b2c125d9290f4e
Chino-sama <beto4600@gmail.com>
2019-11-22 20:38:11 -0600

    feat: added carrousel in heroDetail

--- src/Routes/HeroesDetailRoute.js
+++ src/Routes/HeroesDetailRoute.js
@@ -16,16 +16,20 @@ import ChipsInput from '../Components/ChipsInput';
 import RadioTabs from '../Components/RadioTabs';
 import HeroSuit from '../Components/HeroSuit';
 import SimpleCard from '../Components/SimpleCard';
+import ItemsCarousel from 'react-items-carousel';
 
 import { fetchSuperhero, updateSuperhero, deleteSuperhero } from '../actions/SuperheroesActions';
 import { setChips } from '../actions/ChipsActions';
+import { ChevronLeft, ChevronRight, X } from 'react-feather';
 
 export default function HeroesDetailRoute() { 
 	const dispatch = useDispatch();
 	const history = useHistory();
 	const { id } = useParams();
 	const [isEditOpen, setEditOpen] = useState(false);
-	const [isDeleteHero, setDeleteHero] = useState(false);
+	const [isDeleteHeroOpen, setDeleteHero] = useState(false);
+	const [isDeleteSuitOpen, setDeleteSuit] = useState(false);
+	const [activeSuit, setActiveSuit] = useState(0);
 	const name = useFormInput('');
 	const powers = useFormValue([]);
 	const weather = useFormValue([]);
@@ -42,6 +46,25 @@ export default function HeroesDetailRoute() {
 		{ label: 'Tierra', value: 'tierra' },
 		{ label: 'Aire', value: 'aire' }
 	]
+
+	const Arrow = (isLeft) => {
+		if (isLeft)
+			return (
+				<ChevronLeft
+					className='clickable'
+					size={38}
+					color='#EF2626'
+				/>
+			);
+		else
+			return (
+				<ChevronRight
+					className='clickable'
+					size={38}
+					color='#EF2626'
+				/>
+			);
+	}
 
 	useEffect(() => {
 		if(!superhero) {
@@ -107,26 +130,67 @@ export default function HeroesDetailRoute() {
 					</div>
 				</div>
 				<div className='flex'>
-					<SimpleCard className='flex flex05 silhouette-container justify-content-center small-padding-vertical huge-margin-horizontal'>
-						<img
-							height='100%'
-							src={superhero.suits[0].thumbnail}
-						/>
-					</SimpleCard>
+					<div className='flex flex05 silhouette-container justify-content-center big-margin-horizontal'>
+						<ItemsCarousel
+							infiniteLoop={false}
+							activeItemIndex={activeSuit}
+							requestToChangeActive={(value) => setActiveSuit(value)}
+							numberOfCards={1}
+							slidesToScroll={1}
+							rightChevron={Arrow()}
+							leftChevron={Arrow(true)}
+						>
+							{
+								superhero.suits.map((item, index) =>
+									<SimpleCard
+										key={index}
+										className='small-margin-vertical huge-margin-left small-padding-vertical silhouette-container full-width flex justify-content-center width25'
+									>
+										<div className='full-height'>
+											<img
+												height='100%'
+												src={item.thumbnail}
+											/>
+											<div className='delete-btn'>
+												<PillBtn
+													className='small centered'
+													text={
+														<X color='white' size='21' />
+													}
+													background='#989898'
+													onClick={() => setDeleteSuit(true)}
+												/>
+											</div>
+										</div>
+									</SimpleCard>
+								)
+							}
+						</ItemsCarousel>
+					</div>
 					<div className='flex1 column'>
 						<p className='big-text no-margin-top small-margin-bottom'>Poderes</p>
 						<div className='flex'>
 							{
-								superhero.superpowers.map((item) =>
-									<div className='tag'>{item}</div>
+								superhero.superpowers.map((item, index) =>
+									<div 
+										key={index}
+										className='tag'
+									>
+										{item}
+									</div>
 								)
 							}
 						</div>
 						<p className='big-text margin-top small-margin-bottom'>Clima</p>
 						<div className='flex'>
 							{
-								superhero.weather.map((item) =>
-									<div className='tag'>{item}</div>
+								superhero.weather.map((item, index) =>
+									<div 
+										key={index}
+										className='tag'
+									>
+										{item}
+									</div>
 								)
 							}
 						</div>
@@ -141,7 +205,7 @@ export default function HeroesDetailRoute() {
 					onClick={() => history.push('/create-suit', { id: superhero._id })}
 				/>
 				<CustomModal
-					isOpen={isDeleteHero}
+					isOpen={isDeleteHeroOpen}
 					smallModal
 					onRequestClose={() => setDeleteHero(false)}
 				>
@@ -160,6 +224,30 @@ export default function HeroesDetailRoute() {
 								text={isLoading ? 'Borrando...' : 'Confirmar'}
 								background='#000'
 								onClick={() => deleteSup()}
+							/>
+						</div>
+					</div>
+				</CustomModal>
+				<CustomModal
+					isOpen={isDeleteSuitOpen}
+					smallModal
+					onRequestClose={() => setDeleteSuit(false)}
+				>
+					<div className='flex column full-height align-items-center justify-content-space-around'>
+						<p className='huge-text no-margin'>ELIMINAR TRAJE</p>
+						<p className='big-text no-margin'>¿Estás segura?</p>
+						<div className='flex full-width justify-content-space-between'>
+							<PillBtn
+								className='flex02 align-self-flex-end'
+								text='Cancelar'
+								left
+								onClick={() => setDeleteSuit(false)}
+							/>
+							<PillBtn
+								className='flex02 align-self-flex-end'
+								text={isLoading ? 'Borrando...' : 'Confirmar'}
+								background='#000'
+								onClick={() => {}}
 							/>
 						</div>
 					</div>
